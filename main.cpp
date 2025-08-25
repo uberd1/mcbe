8df7199df1922bf9eea3184c97b9d3b691e2b0ae
uberd1 <mishaosipov950@gmail.com>
2025-08-25 14:32:21 +0300

    Cmake + main.cpp changes

--- main.cpp
+++ main.cpp
@@ -2,26 +2,35 @@
 
 #include "mce/platform/DeviceInfo.h"
 #include "mce/client/gui/screen/SettingsScreen.h"
+#include "mce/common/sound/SoundManager.h"
+#include "mce/render/GraphicsDevice.h"
+
+struct GraphicsInfo;
+GraphicsInfo* GetGlobalGraphicsInfo();
 
 int main()
 {
     std::cout << "--- Testing DeviceInfo ---" << std::endl;
-
     DeviceInfo myDevice;
-
-    std::cout << "Platform: " << myDevice.platformName << std::endl;
     std::cout << "Device ID: " << myDevice.deviceId << std::endl;
-    std::cout << "IP Address: " << myDevice.ipAddress << std::endl;
 
     std::cout << "\n--- Testing SettingsScreen ---" << std::endl;
-
     SettingsScreen mySettingsScreen;
-
     std::cout << "SettingsScreen object created successfully!" << std::endl;
 
-    std::cout << "\nSuccess! Press Enter to exit." << std::endl;
+    std::cout << "\n--- Testing SoundManager ---" << std::endl;
+    SoundManager soundManager;
+    std::cout << "Dispatching sound event 'ui.button.press'..." << std::endl;
+    soundManager.playSoundEvent("ui.button.press", false, false, 1.0f, 1.0f);
+    std::cout << "Sound event dispatched successfully!" << std::endl;
 
-    std::cin.get();
+    std::cout << "\n--- Testing GraphicsDevice ---" << std::endl;
 
-    return 0;
-}
+    GraphicsInfo* graphicsInfo = GetGlobalGraphicsInfo();
+    std::cout << "Successfully got GraphicsInfo object." << std::endl;
+
+    GraphicsDevice graphicsDevice;
+    bool success = graphicsDevice.Initialize(graphicsInfo);
+
+    if (success) {
+        std::cout << "GraphicsDevice initialized successf
